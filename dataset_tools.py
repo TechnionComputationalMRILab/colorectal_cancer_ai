1f3a133eb057989b0daa9aa88d47db48bec261c9
shatz <danielshats1@gmail.com>
2021-11-29 15:29:28 +0200

    dataloader with paths

---
+++ dataset_tools.py
@@ -0,0 +1,84 @@
+import numpy as np
+import pandas as pd
+import torch
+import torchvision
+from tqdm.notebook import tqdm
+import glob
+import re
+
+
+from torchvision.datasets.folder import default_loader
+from torchvision.datasets.folder import Optional
+from torchvision.datasets.folder import Callable
+from torchvision.datasets.folder import Any
+from torchvision.datasets.folder import Tuple
+from torchvision.datasets.folder import ImageFolder
+from torchvision.datasets.folder import IMG_EXTENSIONS
+
+
+class ImageFolderWithPaths(torchvision.datasets.DatasetFolder):
+    """	
+		This class is created to also return the image path of each sample.
+		patient_eval_dataset[idx] = (path,img,target)
+	
+		## Below are notes from pytorch docs on torchvision.datasets.ImageFolder ##
+		A generic data loader where the images are arranged in this way by default: ::
+
+        root/dog/xxx.png
+        root/dog/xxy.png
+        root/dog/[...]/xxz.png
+
+        root/cat/123.png
+        root/cat/nsdf3.png
+        root/cat/[...]/asd932_.png
+
+    This class inherits from :class:`~torchvision.datasets.DatasetFolder` so
+    the same methods can be overridden to customize the dataset.
+
+    Args:
+        root (string): Root directory path.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        loader (callable, optional): A function to load an image given its path.
+        is_valid_file (callable, optional): A function that takes path of an Image file
+            and check if the file is a valid file (used to check of corrupt files)
+
+     Attributes:
+        classes (list): List of the class names sorted alphabetically.
+        class_to_idx (dict): Dict with items (class_name, class_index).
+        imgs (list): List of (image path, class_index) tuples
+    """
+
+    def __init__(
+            self,
+            root: str,
+            transform: Optional[Callable] = None,
+            target_transform: Optional[Callable] = None,
+            loader: Callable[[str], Any] = default_loader,
+            is_valid_file: Optional[Callable[[str], bool]] = None,
+    ):
+        super(ImageFolderWithPaths, self).__init__(root, loader, IMG_EXTENSIONS if is_valid_file is None else None,
+                                          transform=transform,
+                                          target_transform=target_transform,
+                                          is_valid_file=is_valid_file)
+        self.imgs = self.samples
+        
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (path, sample, target) where target is class_index of the target class.
+        """
+        path, target = self.samples[index]
+        sample = self.loader(path)
+        if self.transform is not None:
+            sample = self.transform(sample)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return path, sample, target
+
