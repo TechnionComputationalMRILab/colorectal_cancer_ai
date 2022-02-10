55bedb348ce92f2959e5b4100d599ac5f35d35a3
Daniel Shats <shats@dgx-master.technion.ac.il>
2022-02-10 19:01:05 +0200

    added parallel ddp

--- dp_moco_experiment.py
+++ dp_moco_experiment.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 # coding: utf-8
-# from src.data_stuff.pip_tools import install
-# install(["pytorch-lightning", "seaborn", "timm", "wandb", "plotly", "lightly"], quietly=True) #opencv-python too
+
+ON_SERVER = "DGX"
+# ON_SERVER = "moti"
+# ON_SERVER = "Alsx2"
+
+data_dir=None
+checkpoint_dir = "./saved_models/"
+if ON_SERVER == "DGX":
+    data_dir="/workspace/repos/TCGA/data/"
+    checkpoint_dir = "/workspace/repos/colorectal_cancer_ai/saved_models/"
+    from src.data_stuff.pip_tools import install
+    install(["pytorch-lightning", "seaborn", "timm", "wandb", "plotly", "lightly"], quietly=True)
+elif ON_SERVER == "moti":
+    data_dir="/home/shats/data/data/"
+elif ON_SERVER == "Alsx2":
+    data_dir="/home/shats/data/data/"
+
 import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
@@ -11,27 +26,21 @@ from src.data_stuff import tcga_datamodules, tcga_moco_dm
 from src.model_stuff import moco_model
 from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 
-# ON_SERVER = "DGX"
-# ON_SERVER = "moti"
-ON_SERVER = "Alsx2"
 
-data_dir=None
-checkpoint_dir = "./saved_models/"
-if ON_SERVER == "DGX":
-    data_dir="/workspace/repos/TCGA/data/"
-    checkpoint_dir = "/workspace/repos/colorectal_cancer_ai/saved_models/"
-elif ON_SERVER == "moti":
-    data_dir="/home/shats/data/data/"
-elif ON_SERVER == "Alsx2":
-    data_dir="/home/shats/data/data/"
+# --- hypers --- #
+data_subset=0.5
+batch_size=1024
+num_nodes=1
+gpus=2
+num_workers=16
+strat="ddp_cpu"
+# ------------- #
 
-
-data_subset=None
-batch_size=32
-EXP_NAME = f"tcga_MOCO_{ON_SERVER}_datasz{data_subset}_bs{batch_size}"
+EXP_NAME = f"tcga_MOCO_{ON_SERVER}_datasz{data_subset}_bs{batch_size}_gpus{gpus}_nodes{num_nodes}"
 logger = WandbLogger(project="moti", name=EXP_NAME)
 
 print("---- MOCO Experiment (tcga) ----")
+print(f"experiment name: {EXP_NAME}")
 print('CUDA available:', torch.cuda.is_available())
 
 memory_bank_size = 4096
@@ -39,7 +48,10 @@ moco_max_epochs = 900
 # downstream_max_epochs = 60
 # downstream_test_every = 50
 model = moco_model.MocoModel(memory_bank_size, moco_max_epochs)
-dm = tcga_moco_dm.MocoDataModule(data_dir=data_dir,batch_size=batch_size, subset_size=data_subset)
+dm = tcga_moco_dm.MocoDataModule(data_dir=data_dir,
+        batch_size=batch_size, 
+        subset_size=data_subset,
+        num_workers=16)
 
 
 # monitors
@@ -53,17 +65,20 @@ checkpoint_callback = ModelCheckpoint(
 )
 lr_monitor = LearningRateMonitor(logging_interval='step')
 
-trainer = Trainer(gpus=1, max_epochs=moco_max_epochs,
+trainer = Trainer(gpus=gpus,
+        num_nodes=num_nodes,
+        strategy=strat,
+        max_epochs=moco_max_epochs,
         logger=logger,
         callbacks=[
             # LogConfusionMatrix.LogConfusionMatrix(class_to_idx),
-            DownstreamTrainer2.DownstreamTrainer(data_dir=data_dir,
-                downstream_max_epochs=20,
-                downstream_subset_size=data_subset,
-                do_first_n_epochs=2,
-                do_every_n_epochs=200,
-                downstream_batch_size=batch_size,
-                do_on_train_epoch_end=True),
+            # DownstreamTrainer2.DownstreamTrainer(data_dir=data_dir,
+            #     downstream_max_epochs=20,
+            #     downstream_subset_size=data_subset,
+            #     do_first_n_epochs=2,
+            #     do_every_n_epochs=200,
+            #     downstream_batch_size=batch_size,
+            #     do_on_train_epoch_end=True),
             checkpoint_callback,
             lr_monitor
             ],
